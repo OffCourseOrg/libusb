432fc82b0104a5e1ede43a0f1e0b95cf4dadc5ab
onixtsm <alexander@sautkin.com>
2025-04-28 23:18:22 +0200

    Fixed bug with incremental build

--- build.sh
+++ build.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 set xe
 
-if [ ! -f "./configure.ac" ] || [ ! -f "./configure" ] || [ ! -f "./Makefile" ] || [ "./bootstrap.sh" -nt "./configure" ] || [ "./bootstrap.sh" -nt "./configure.ac" ] || [ "./Makefile" -nt "./configure" ]; then
+if [ ! -f "./configure.ac" ] || [ ! -f "./configure" ] || [ ! -f "./Makefile" ] || [ "./bootstrap.sh" -nt "./configure" ] || [ "./bootstrap.sh" -nt "./configure.ac" ] || [ "./configure" -nt "./Makefile" ]; then
   ./bootstrap.sh
   emconfigure ./configure --host=wasm32-emscripten --disable-shared
   touch Makefile config.status
+  make clean
 else
   echo Do not reconfigure
 fi
