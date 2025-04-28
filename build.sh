7a1233c9b6d4b3a31c9e4a2a15b1ef71d7aa1dfc
onixtsm <alexander@sautkin.com>
2025-04-28 23:05:52 +0200

    Changed build.sh to not configure every time

--- build.sh
+++ build.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
-set x
+set xe
 
-./bootstrap.sh
-emconfigure ./configure --host=wasm32-emscripten --disable-shared --prefix=$PWD
+if [ ! -f "./configure.ac" ] || [ ! -f "./configure" ] || [ ! -f "./Makefile" ] || [ "./bootstrap.sh" -nt "./configure" ] || [ "./bootstrap.sh" -nt "./configure.ac" ] || [ "./Makefile" -nt "./configure" ]; then
+  ./bootstrap.sh
+  emconfigure ./configure --host=wasm32-emscripten --disable-shared
+  touch Makefile config.status
+else
+  echo Do not reconfigure
+fi
 emmake make LDFLAGS="USE_WEBUSB=1" -j$(nproc)
